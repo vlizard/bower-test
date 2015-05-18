20cab0752268699e71d599aaca48e6111d49dcda
Valeriy Shkiryak <valeriy.lizard@gmail.com>
2015-05-18 13:54:51 +0300

    Use mocha test framework

--- tests/index.js
+++ tests/index.js
@@ -1,29 +1,22 @@
+var expect = chai.expect;
 describe("dummyExtend", function() {
 	it("should include defaults", function() {
-		expect(dummyExtend({x: 1})).toEqual(jasmine.objectContaining({
-			y: -2,
-			z: -3
-		})); 
+		expect(dummyExtend({x: 1})).to.have.property("z", -3);
 	});
 
 	it("should redefine values", function() {
-		expect(dummyExtend({x: 1})).toEqual(jasmine.objectContaining({
-			x: 1
-		})); 
+		expect(dummyExtend({x: 1})).to.have.property("x", 1);
 	});
 
 	it("should redefine nested values", function() {
-		expect(dummyExtend({meta: {desc: "New desc"}})).toEqual(jasmine.objectContaining({
-			meta: {
-				desc: "New desc"
-			}
-		})); 
+		expect(dummyExtend({meta: {desc: "New desc"}}))
+			.to.have.deep.property("meta.desc", "New desc");
 	});
 
 	it("should execute $.extend", function() {
 		var spy = sinon.spy($, "extend");
 		dummyExtend({});
-		expect(spy.called).toBe(true);
+		expect(spy.called).to.be.true;
 		spy.restore();
 	});
 });
